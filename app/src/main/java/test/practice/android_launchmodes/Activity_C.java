7c17e8f57bc1b4e40cdfc2526e654c3679c3476c
Parasmani Sharma <parassharma1805@gmail.com>
2017-11-03 17:56:04 +0530

    Design Pattern, Other Update.

--- app/src/main/java/test/practice/android_launchmodes/Activity_C.java
+++ app/src/main/java/test/practice/android_launchmodes/Activity_C.java
@@ -23,7 +23,7 @@ public class Activity_C extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent obj = new Intent(Activity_C.this, Activity_B.class);
+                Intent obj = new Intent(Activity_C.this, Activity_D.class);
                 startActivity(obj);
             }
         });
--- javapractice/src/main/java/com/example/logicprogram/oops/AbstractionExample.java
+++ javapractice/src/main/java/com/example/logicprogram/oops/AbstractionExample.java
@@ -7,28 +7,32 @@ import java.util.Scanner;
  */
 
 /*
+* Abstraction :
+*
+* You can create Object of Abstract class but you cant create object of INterface.
+* Why we use : To provide security at coding level we use abstraction.
+*
 * Best example to analyze and think when you talk about Client-Server based interaction using Abstract.
 * This type of interaction we have done in Aidl example - (Aidl_Test_Cover)
 * Try to assume this program as client-server interaction. Here you can consider that Bank Abstract class is client side
-* and its implementation is on server side i.e SBI server.
+* and its implementation is on server side i.e via SBI server.
 * Here we have shown abstract class only at Client Side. But in real world senerio - one Abstract class (with the
 * same name as Client Abstract class) is also present at Server Side (as like client side Abstract class) as we have done in
-* our AIDl_Test_Cover project. By this we can analyze that a pipeline will create for communication between both
+* our AIDl_Test_Cover project(considered Interface in this case). By this we can analyze that a pipeline will create for communication between both
 * Client and Server(acts like mirror - which uses shared memory concept on same device for communication between two
 * Apps using IBinder Interface of Android OS).
 * */
 
 class SBI extends Bank
 {
-    private int cash = 1000000;
+    private int cash = 10000;
     @Override
     public int withdrawelAmount(int i) {
         if(i < 1200)
         {
             System.out.println("Cash withdrawel successfully !!");
             cash = cash-i;
-            System.out.println("\nRemaining Account Balance : " + cash);
-            return i;
+            return cash;
 
         }else {
             System.out.println("\nYou are not allowed");
@@ -59,6 +63,7 @@ class Failed extends Bank
     @Override
     public void getLogin() {
 
+        System.out.println(" Failed");
     }
 
     @Override
@@ -102,10 +107,11 @@ public class AbstractionExample {
 
         System.out.println("Enter Password to login in bank : " );
         Scanner scan = new Scanner(System.in);
-        scan.nextInt();
+        int passsword = scan.nextInt();
 
-        if(scan.nextInt() == 12345)
+        if(passsword == 12345)
         {
+            System.out.println("Your account balance is : 10000" );
             SBI success = new SBI();
             connectBank(success);
         }
@@ -121,10 +127,11 @@ public class AbstractionExample {
     {
         if(obj instanceof SBI)
         {
-
             Bank user1 = (SBI)obj;
             user1.getLogin();
-            user1.withdrawelAmount(1000);
+            int amountLeft = user1.withdrawelAmount(1000);
+            System.out.println("\nRemaining Account Balance : " + amountLeft);
+            System.out.println("\n\n........Session End.......");
         }else
         {
             Bank user1 = (Failed)obj;
